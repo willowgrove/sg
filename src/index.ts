bac5f644951888d3912b561c204904138e2213bd
Sean Wong <seanwong24@live.com>
2023-02-08 05:51:53 +0000

    export html and render from lit

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export * from './components/alert/alert';
 export * from './components/card/card';
 export * from './components/collapse/collapse';
 export * from './components/button/button';
+export {html, render} from 'lit';
